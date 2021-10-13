e94418e0af935051157a9723203681eef01f7b0f
Trevor <trevorj342@gmail.com>
2021-10-13 01:04:00 -0400

    add unity importer, update readme and foldername to reflect changes

---
+++ ImporterSource/UMapTextureApplicator.cs
@@ -0,0 +1,95 @@
+﻿using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+public class UMapTextureApplicator : EditorWindow
+{
+    private string umapPath;
+    private string textureImportDir = "Assets/UMapTextures/";
+    private GameObject rootObject;
+
+    private readonly Dictionary<string, string> textureShaderMapping =
+        new Dictionary<string, string>()
+            {
+                { "Diffuse", "_MainTex" },
+                { "Normal", "_BumpMap" },
+                { "Specular", "" },
+                { "SpecPower", "" },
+                { "Opacity", "" },
+                { "Emissive", "_EmissionMap" },
+                { "Cube", "" },
+                { "Mask", "" }
+            };
+
+    [MenuItem("Tools/UMap Texture Applicator")]
+    public static void OpenWindow()
+        => GetWindow<UMapTextureApplicator>("Texture Applicator");
+
+    private void OnGUI()
+    {
+        GUILayout.Label("UMap Texture Applicator", EditorStyles.centeredGreyMiniLabel);
+        GUILayout.Space(20);
+
+        umapPath = EditorGUILayout.TextField("BlenderUMap Path", umapPath);
+        textureImportDir = EditorGUILayout.TextField("Texture Import Path", textureImportDir);
+        rootObject = (GameObject)EditorGUILayout.ObjectField("Root Object", rootObject, typeof(GameObject), true);
+
+        if (GUILayout.Button("Begin Import"))
+            ImportTextures();
+    }
+
+    private void ImportTextures()
+    {
+        // create import dir
+        if (!Directory.Exists(textureImportDir))
+            Directory.CreateDirectory(textureImportDir);
+
+        // deal with unity's asset system and jankily import all png textures as Texture2Ds
+        List<Texture2D> allTextures = new List<Texture2D>();
+        string[] texturePaths = Directory.GetFiles(umapPath, "*.png", SearchOption.AllDirectories);
+        foreach (string texture in texturePaths)
+        {
+            if (!File.Exists(Path.Combine(textureImportDir, Path.GetFileName(texture))))
+                FileUtil.CopyFileOrDirectory(texture, Path.Combine(textureImportDir, Path.GetFileName(texture)));
+        }
+        AssetDatabase.Refresh();
+        foreach (string texture in texturePaths)
+            allTextures.Add((Texture2D)AssetDatabase.LoadAssetAtPath(Path.Combine(textureImportDir, Path.GetFileName(texture)), typeof(Texture2D)));
+
+        // actually do the texture application stuff
+        string json = File.ReadAllText(Path.Combine(umapPath, "_processed.json"));
+        JArray mainArray = JArray.Parse(json);
+        foreach (var obj in mainArray)
+        {
+            MeshRenderer rend = rootObject.transform.Find(obj["Name"].ToString())?.GetComponent<MeshRenderer>();
+            if (rend == null)
+            {
+                Debug.LogWarning("Failed to find object " + obj["Name"] + ", textures will not be applied");
+                continue;
+            }
+            
+            foreach (var mat in obj["Materials"])
+            {
+                Material realMat = rend.sharedMaterials.SingleOrDefault(m => m.name == mat["Name"].ToString());
+                if (realMat == null)
+                {
+                    Debug.LogWarning("Failed to find material " + mat["Name"] + ", textures will not be applied");
+                    continue;
+                }
+
+                foreach (var mapping in textureShaderMapping)
+                {
+                    if (mapping.Value != string.Empty && mat["Textures"][mapping.Key] != null)
+                    {
+                        realMat.SetTexture(mapping.Value, allTextures.SingleOrDefault(t => t.name == Path.GetFileNameWithoutExtension(mat["Textures"][mapping.Key].ToString())));
+                    }
+                }
+            }
+        }
+
+        AssetDatabase.SaveAssets();
+    }
+}
--- README.md
+++ README.md
@@ -8,13 +8,18 @@ A port of [Amrsatrio/BlenderUmap](https://github.com/Amrsatrio/BlenderUmap) to C
 4. Open `config.json` in any text editor and fill the data as needed.
 5. Run the exe again and wait as it finds and exports the map's actors.
    - You may receive warnings like `Did not read "MaterialInstanceConstant" correctly, X bytes remaining`, this should not impact the export and can be ignored.
-6. Once complete, create a new project in Blender and delete all the starter objects.
-7. Open the Scripting tab and create a new file.
-8. Copy and paste the Python script from [here](https://github.com/TrevTV/BlenderUMap/blob/main/BlenderSource/umap.py) into the text editor.
+
+# Importing into Blender
+1. Create a new project in Blender and delete all the starter objects.
+2. Open the Scripting tab and create a new file.
+3. Copy and paste the Python script from [here](https://github.com/TrevTV/BlenderUMap/blob/main/ImporterSource/blender_import.py) into the text editor.
     - At this point, if you do not have the `import_psk_psa` plugin installed, please download it from [here](https://github.com/Befzz/blender3d_import_psk_psa) and install it.
-9. In the script, change `data_dir` to be where the BlenderUMap exe is located.
-10. Press `Alt + P` to run the script and wait as it imports the meshes and materials.
+4. In the script, change `data_dir` to be where the BlenderUMap exe is located.
+5. Press `Alt + P` to run the script and wait as it imports the meshes and materials.
     - If you would like to view the progress of the import, before starting open the System Console by going to `Window > Toggle System Console`
+
+# Importing into Unity
+TODO
 
 # Credits
  - [Amrsatrio](https://github.com/Amrsatrio/) for the original BlenderUmap
